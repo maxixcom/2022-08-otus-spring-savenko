e4aaf245922472ee980e3d6229c58b74c7b6e148
Maxim Savenko <maxim.savenko@gmail.com>
2022-09-27 11:51:25 +0300

    +shell command

---
+++ lesson-7/hometask-quiz/src/main/java/io/github/maxixcom/otus/quiz/command/QuizCommand.java
@@ -0,0 +1,39 @@
+package io.github.maxixcom.otus.quiz.command;
+
+import io.github.maxixcom.otus.quiz.service.InputOutputService;
+import io.github.maxixcom.otus.quiz.service.QuizRunner;
+import org.springframework.shell.Availability;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
+
+@ShellComponent
+public class QuizCommand {
+    private final QuizRunner quizRunner;
+    private final InputOutputService inputOutputService;
+
+    private Boolean isRegistered = false;
+
+    public QuizCommand(QuizRunner quizRunner, InputOutputService inputOutputService) {
+        this.quizRunner = quizRunner;
+        this.inputOutputService = inputOutputService;
+    }
+
+    @ShellMethod(value = "Запуск тестирования - Quiz", key = {"q", "quiz", "run-quiz"})
+    @ShellMethodAvailability(value = "isRegistered")
+    public void runQuiz() {
+        quizRunner.run();
+    }
+
+    public Availability isRegistered() {
+        return isRegistered
+                ? Availability.available()
+                : Availability.unavailable("\'Вам необходимо зарегистрироваться\'");
+    }
+
+    @ShellMethod(value = "Регистрация на прохождение тестирования", key = {"r", "register"})
+    public void register() {
+        this.isRegistered = true;
+        inputOutputService.printlnString("Вы зарегистрированы");
+    }
+}
