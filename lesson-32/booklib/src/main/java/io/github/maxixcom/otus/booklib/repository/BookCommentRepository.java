34d8b1310894e714100d0ba1768ac480c23a0863
Maxim Savenko <maxim.savenko@gmail.com>
2023-01-01 13:57:11 +0300

    exclude comments from data rest

--- lesson-32/booklib/src/main/java/io/github/maxixcom/otus/booklib/repository/BookCommentRepository.java
+++ lesson-32/booklib/src/main/java/io/github/maxixcom/otus/booklib/repository/BookCommentRepository.java
@@ -2,6 +2,8 @@ package io.github.maxixcom.otus.booklib.repository;
 
 import io.github.maxixcom.otus.booklib.domain.BookComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource(exported = false)
 public interface BookCommentRepository extends JpaRepository<BookComment, Long> {
 }
