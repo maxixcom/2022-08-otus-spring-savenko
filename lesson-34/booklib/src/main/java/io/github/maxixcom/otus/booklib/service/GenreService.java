9d452cfe18ed35171b60dafb00b298557f3010b0
Maxim Savenko <maxim.savenko@gmail.com>
2023-01-15 19:40:18 +0300

    lesson-34-hometask

---
+++ lesson-34/booklib/src/main/java/io/github/maxixcom/otus/booklib/service/GenreService.java
@@ -0,0 +1,10 @@
+package io.github.maxixcom.otus.booklib.service;
+
+import io.github.maxixcom.otus.booklib.domain.Genre;
+import io.github.maxixcom.otus.booklib.dto.GenreDto;
+
+import java.util.List;
+
+public interface GenreService {
+    List<GenreDto> getAllGenres();
+}
